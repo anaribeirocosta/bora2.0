34e4e19952169423454e9190ee73a5bf4c9943bc
Ana Ribeiro Costa <ana.r.dacosta@gmail.com>
2018-08-19 22:05:07 -0300

    Colocar calendário

--- bora2/calendario.js
+++ bora2/calendario.js
@@ -4,13 +4,18 @@ let chamar = document.querySelector(".chamar");
 let amigos = document.querySelectorAll(".amigos");
 let botoesCalendario = document.querySelectorAll(".botao")
 let calendario = document.querySelector(".calendario");
+let meusDias = document.querySelector("#um");
+let meusAmigos = document.querySelector(".semana")
+let evento = document.querySelector("#evento")
 
 botao.addEventListener('click', function(){
     modal.style.display = 'flex';
 });
 
 chamar.addEventListener('click', function(){
+    evento.style.backgroundColor = "#e25f5f";
     modal.style.display = 'none';
+    
 });
 
 for (botaoCalendario of botoesCalendario){
@@ -18,7 +23,7 @@ for (botaoCalendario of botoesCalendario){
 }
 
 function trocaCalendario(){
-    calendario.style.backgroundColor = 'red';
+    meusDias.style.backgroundColor = "#5fff9c";
     this.style.backgroundColor = '#ff0086';
 }
 
